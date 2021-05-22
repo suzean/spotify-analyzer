8065653b364b25491b9c30d6df78660d8a1cd0b2
suzean <zuzanna.walasek@gmail.com>
2021-05-22 16:02:46 +0200

    new

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=1e3e995ec8cb42969e081568d4bb6a74&response_type=token&redirect_uri=http://localhost:3000&state=123&show_dialog=true&scope=streaming%20user-read-email%20user-read-private%20user-modify-playback-state%20user-read-playback-state%20user-read-recently-played%20playlist-modify-public%20user-top-read%20user-library-modify%20user-library-read%20";
 
 export default function Login() {
+  const location = window.location.href
+  console.log(location);
+
+  const AUTH_URL =
+  `https://accounts.spotify.com/authorize?client_id=1e3e995ec8cb42969e081568d4bb6a74&response_type=token&redirect_uri=${location}&state=123&show_dialog=true&scope=streaming%20user-read-email%20user-read-private%20user-modify-playback-state%20user-read-playback-state%20user-read-recently-played%20playlist-modify-public%20user-top-read%20user-library-modify%20user-library-read%20`;
+
+
   return <div className="main-login">
       <h1 className="title-login"> Track Analyzer</h1>
       <a className="btn-login" href={AUTH_URL}> Login with <span>Spotify</span> </a>
