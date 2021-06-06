17da7f0c6bce0dbe1b9175cc54fe149845117d13
suzean <zuzanna.walasek@gmail.com>
2021-06-06 18:57:07 +0200

    redirect uri update

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,7 @@ export default function Login() {
   console.log(location);
 
   const AUTH_URL =
-  `https://accounts.spotify.com/authorize?client_id=1e3e995ec8cb42969e081568d4bb6a74&response_type=token&redirect_uri=${location}&state=123&show_dialog=true&scope=streaming%20user-read-email%20user-read-private%20user-modify-playback-state%20user-read-playback-state%20user-read-recently-played%20playlist-modify-public%20user-top-read%20user-library-modify%20user-library-read%20`;
+  `https://accounts.spotify.com/authorize?client_id=1e3e995ec8cb42969e081568d4bb6a74&response_type=token&redirect_uri=https://trackanalyzer.netlify.app&state=123&scope=streaming%20user-read-email%20user-read-private%20user-modify-playback-state%20user-read-playback-state%20user-read-recently-played%20playlist-modify-public%20user-top-read%20user-library-modify%20user-library-read%20`;
 
 
   return <div className="main-login">
